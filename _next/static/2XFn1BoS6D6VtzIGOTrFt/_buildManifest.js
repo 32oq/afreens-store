fefe550b4952e59f0363ba3b82095b6c0f591bb8
Shaista Akhter <peerzadaowais36@gmail.com>
2026-06-19 17:56:59 +0530

    Updates

---
+++ _next/static/2XFn1BoS6D6VtzIGOTrFt/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/afreens-store//_next/:path+",
+        "destination": "/afreens-store/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
